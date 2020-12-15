78f637cdef85acc908bb6ea88aa9ae96ceef4383
Thomas <thom.laffay@gmail.com>
2020-12-15 15:36:23 +0100

    Ajouté des boules, fix pour les portes

---
+++ Assets/Boulasse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boulasse : MonoBehaviour
+{
+    Rigidbody bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float speed = 4f;
+
+        Vector3 currentPosition = transform.position;
+
+        Vector3 moveForward = transform.forward;
+
+        Vector3 newPosition = currentPosition + moveForward * speed * Time.deltaTime;
+
+        bullet.MovePosition(newPosition);
+    }
+}
---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 position = transform.position + Vector3.up * 0.5f + transform.forward * 0.5f;
+            Instantiate(Sphere, position, transform.rotation);
+        }
+    }
+}
