9ce78782f9e13cd3eafa01fe92bcff7231abfdd9
Thomas <thom.laffay@gmail.com>
2020-12-16 17:02:14 +0100

    Ajouté le script de ouf

---
+++ Assets/ResetPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 positionDeDepart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionDeDepart = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y <= -1f)
+        {
+            transform.position = positionDeDepart;
+        }
+    }
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    Rigidbody lobjet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lobjet = GetComponent<Rigidbody>();
+        lobjet.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        Debug.Log("Collision normale");
+        if (collision.gameObject.CompareTag("Sphere"))
+        {
+            Debug.Log("Collision sphere");
+            lobjet.isKinematic = false;
+        }
+    }
+}
