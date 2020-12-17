9b2033507f46c89a1dec3d89c20d3381055ca8e5
Thomas <thom.laffay@gmail.com>
2020-12-17 15:42:02 +0100

    Ajouté le score

--- Assets/Disparition.cs
+++ Assets/Disparition.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Disparition : MonoBehaviour
 {
-
     Rigidbody lobjet;
     public float numberOfCollisions;
+    Score scoreObject;
+
+    [SerializeField]
+    int scoreDeTouche = 5;
+    [SerializeField]
+    int scoreDeDestruction = 10;
+    [SerializeField]
+    int nombreDeHp = 5;
 
     // Start is called before the first frame update
     void Start()
     {
         lobjet = GetComponent<Rigidbody>();
         numberOfCollisions = 0;
+        scoreObject = FindObjectOfType<Score>();
     }
 
     // Update is called once per frame
@@ -22,14 +29,16 @@ public class Disparition : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Sphere") && numberOfCollisions < 5)
+        if (collision.gameObject.CompareTag("Sphere") && numberOfCollisions < nombreDeHp)
         {
             Debug.Log(numberOfCollisions);
             numberOfCollisions++;
+            scoreObject.changeScore(scoreDeTouche);
         } 
-        else if(collision.gameObject.CompareTag("Sphere") && numberOfCollisions >= 5)
+        else if(collision.gameObject.CompareTag("Sphere") && numberOfCollisions >= nombreDeHp)
         {
             Destroy(gameObject);
+            scoreObject.changeScore(scoreDeDestruction);
         }
     }
 }
---
+++ Assets/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    float score;
+    // Start is called before the first frame update
+    public Text leTexte;
+    void Start()
+    {
+        leTexte = GetComponent<Text>();
+        leTexte.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeScore(int modif)
+    {
+        score = score + modif;
+        leTexte.text = score.ToString();
+    }
+}
